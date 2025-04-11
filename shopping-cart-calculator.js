620b16ad049964beef6853abd2b2377a10534a27
alexscespedes <alexandersenc99@gmail.com>
2025-04-11 04:34:33 -0400

    Create the array of product objects

---
+++ shopping-cart-calculator.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const products = [
+  { name: "Dell Inspiron", price: 500, quantity: 5 },
+  { name: "Headphone SONY", price: 200, quantity: 25 },
+  { name: "Black Puma T-shirt", price: 25, quantity: 55 },
+];
+
+const calculateCartTotal = function () {};
