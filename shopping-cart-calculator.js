9c3effa42476be42257f4912c9c3e9fb45dc01ac
alexscespedes <alexandersenc99@gmail.com>
2025-04-17 15:40:06 -0400

    Add input validation to handle empty arrays or invalid products

--- shopping-cart-calculator.js
+++ shopping-cart-calculator.js
@@ -6,9 +6,33 @@ const products = [
   { name: "Black Puma T-shirt", price: 27.39, quantity: 55 },
 ];
 
+const testProducts = [
+  { name: "Dell Inspiron", price: 66.36, quantity: 99 },
+  { name: "Headphone SONY", price: 289.99, quantity: 50 },
+  { name: "Black Puma T-shirt", price: 27.39, quantity: 55 },
+];
+
 const roundToTwo = (num) => Number(Math.round((num * 100) / 100).toFixed(2));
 
 const calculateCartTotal = function (data) {
+  if (data.length === 0) {
+    return "The array is empty.";
+  }
+  // if is not a number
+  // if is less than 0
+  for (const product of data) {
+    if (
+      typeof product.price !== "number" ||
+      typeof product.quantity !== "number"
+    ) {
+      return "Error: Invalid product data.";
+    }
+
+    if (product.price < 0 || product.quantity < 0) {
+      return "Error: Price and quantity must be non-negative and positive.";
+    }
+  }
+
   const subTotal = data
     .map((product) => product.price * product.quantity)
     .reduce((acc, value) => acc + value);
@@ -19,11 +43,7 @@ const calculateCartTotal = function (data) {
     salesTax: roundToTwo(salesTax),
     total: roundToTwo(total),
   };
-  // console.log(roundToTwo(subTotal));
-  // console.log(roundToTwo(salesTax));
-  // console.log(roundToTwo(total));
   return object;
 };
 
-console.log(products);
-console.log(calculateCartTotal(products));
+console.log(calculateCartTotal(testProducts));
